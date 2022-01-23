dc741bcf76dcdf2670e918d039ecb860a2aabacc
Sanjoy Das <sanjoy@playingwithpointers.com>
2022-01-23 11:48:26 -0800

    Remap script OSX

---
+++ scripts/remap-osx-keys-kinesis.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+KINESIS_PRODUCT_ID="0x9410"
+KB_LEFT_ALT="0x7000000E2"
+KB_LEFT_GUI="0x7000000E3"
+
+hidutil property --matching "{\"ProductID\":${KINESIS_PRODUCT_ID}}" --set "
+{\"UserKeyMapping\": 
+ [
+  {\"HIDKeyboardModifierMappingSrc\":${KB_LEFT_ALT},
+   \"HIDKeyboardModifierMappingDst\":${KB_LEFT_GUI}},
+  {\"HIDKeyboardModifierMappingSrc\":${KB_LEFT_GUI},
+   \"HIDKeyboardModifierMappingDst\":${KB_LEFT_ALT}}
+]}"
