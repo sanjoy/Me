654238555959fb49f57bb4322cdaa5e4cd9de13d
Sanjoy Das <sanjoy@playingwithpointers.com>
2022-01-29 13:36:20 -0800

    New script: setup links

---
+++ scripts/setup-links.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd $HOME
+
+ln -s Code/Me/condarc .condarc
+ln -s Code/Me/Emacs/ .emacs.d
+ln -s Code/Me/gitconfig .gitconfig
+ln -s Code/Me/tmux.conf .tmux.conf
+ln -s Code/Me/zlogin .zlogin
+ln -s Code/Me/zshrc .zshrc
