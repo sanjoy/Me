a2a13f85ff33933f3083358acd0063d1d47e981b
Sanjoy Das <sanjoy@playingwithpointers.com>
2022-01-31 10:48:17 -0800

    exit code for osx-notify

--- scripts/osx-notify.sh
+++ scripts/osx-notify.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 time $@
+exit_code=$?
 
-osascript -e "display notification \"\\\"$@\\\" exited with $?\" with title \"Finished execution\""
+osascript -e "display notification \"\\\"$@\\\" exited with $exit_code\" with title \"Finished execution\""
+
+exit $exit_code
